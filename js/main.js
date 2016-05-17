f027ecd1bb39e8a4f8dc572dd4a6acaca8ebfe53
stacielove <stacie@love.com>
2016-05-17 23:45:13 +1000

    Added markup, css and JS to create hamburger icon LHS of portfolio-slide, also pops out with nav li's - still trying to fix bug when you scroll back to landing page, and fix transition effects when navigation menu is closed.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-! function() {
+$(document).ready(function() {
   $(".sub-title").typed({
           strings: ["a web developer.", "a systems engineer.", "a network engineer.", "an aspiring devops engineer."],
           typeSpeed: 1,
@@ -6,4 +6,22 @@
           backDelay: 1400,
           loop: !0
       });
-}();
+
+// Added function that adds "open" class to nav bar - need to look at menu-overlay now
+  $("#nav-icon").click(function() {
+        $(this).toggleClass('open'), $("#menu-overlay").toggleClass("menu-show")
+    });
+  // Add a function here that removes "menu-show" class from menu-overlay IF portfolio-slide has class modify - maybe I can use classie - like I did for the nav bar 
+
+  // $(window).scroll(function(){
+    // this part does not work :(
+    // if($(!'.portfolio-slide').hasClass('modify')){
+      // $('#menu-overlay').removeClass('menu-show');
+      // console.log('yay!');
+    // }
+  // });
+
+
+});
+
+
